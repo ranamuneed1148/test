d4458ddb1a1b41c258481ed87f3b7608e8e2f660
Rana Muneeb <ranamuneeb.1148@gmail.com>
2025-07-26 12:23:26 -0700

    welcome

---
+++ index.py
@@ -0,0 +1,10 @@
+print("===================")
+a = input("Enter a Number: ")
+b = input("Enter 2nd Number: ")
+c = input("Enter Operation: ")
+
+if(c == "+"):
+    print(a+b)
+else:
+    if(c == "-"):
+        print(a-b)
